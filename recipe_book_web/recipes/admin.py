de9611afebaa4fd90f4af9bf20040ab8b747adb9
stevb89 <sb.ghiridone@hotmail.com>
2023-01-13 15:29:13 +0100

    up to slide 42 inclusive

--- recipe_book_web/recipes/admin.py
+++ recipe_book_web/recipes/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient
+
 
 # Register your models here.
+admin.site.register(Ingredient)
+
+class IngredientInlineAdmin(admin.StackedInline):
+    model = Ingredient
+    show_change_link = True
+    extra=0
+    fields = (('name', 'quantity'),)
+    
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created', 'difficulty', 'instructions', 'hints')
+    inlines = (IngredientInlineAdmin,)
